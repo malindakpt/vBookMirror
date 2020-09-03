ba8df362440d54c37f928bca29ef2c40dcea37c0
Malinda Kumarasinghe <malindakpt@gmail.com>
2020-09-03 22:38:37 +0530

    Read routing from config

--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -16,84 +16,41 @@ import { AddExam } from '../container/manageCourse/addExam/AddExam';
 import { AddTeacher } from '../container/manageCourse/addTeacher/AddTeacher';
 import { AddCourse } from '../container/manageCourse/addCourse/AddCourse';
 
+const routes = [
+  ['/', Exams],
+  ['/addLesson', AddLesson],
+  ['/editExam', EditExam],
+  ['/addExam', AddExam],
+  ['/addCourse', AddCourse],
+  ['/addTeacher', AddTeacher],
+  ['/addSubject', AddSubject],
+  ['/:examId', Subjects],
+  ['/:examId/:subjectId', Courses],
+  ['/:examId/:subjectId/:courseId', Course],
+];
+
 const Router: React.FC = () => (
   <>
     <BreadcrumbBar />
     <div className={classes.container}>
-
       <div>
         {/* A <Switch> looks through its children <Route>s and
       renders the first one that matches the current URL. */}
         <Switch>
-          <Route
-            exact
-            path="/"
-          >
-            <Exams />
-          </Route>
-          <Route
-            exact
-            path="/addLesson"
-          >
-            <AddLesson />
-          </Route>
-          <Route
-            exact
-            path="/editExam"
-          >
-            <EditExam />
-          </Route>
-
-          <Route
-            exact
-            path="/addCourse"
-          >
-            <AddCourse />
-          </Route>
-          <Route
-            exact
-            path="/addTeacher"
-          >
-            <AddTeacher />
-          </Route>
-          <Route
-            exact
-            path="/addSubject"
-          >
-            <AddSubject />
-          </Route>
-          <Route
-            exact
-            path="/addExam"
-          >
-            <AddExam />
-          </Route>
-          <Route
-            exact
-            path="/:examId"
-          >
-            <Subjects />
-          </Route>
-          <Route
-            exact
-            path="/:examId/:subjectId"
-          >
-            <Courses />
-          </Route>
-          <Route
-            exact
-            path="/:examId/:subjectId/:courseId"
-          >
-            <Course />
-          </Route>
+          {routes.map((r: any) => (
+            <Route
+              exact
+              key={r[0]}
+              path={r[0]}
+              component={r[1]}
+            />
+          ))}
           <Route path="">
             <h3>404</h3>
           </Route>
         </Switch>
       </div>
-
     </div>
-
   </>
 );
 
