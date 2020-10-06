506bfb7891ae33cd2d6b8dd0be185c4d1f687b9c
Malinda Kumarasinghe <malindakpt@gmail.com>
2020-10-06 22:06:26 +0530

    Added new firebase

--- src/data/Config.ts
+++ src/data/Config.ts
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+const firebaseConfigDev = {
   apiKey: 'AIzaSyC5pq-lAQWmVEVmrI0iwQ2bUJvSKp4tXaY',
   authDomain: 'smsm-54fa3.firebaseapp.com',
   databaseURL: 'https://smsm-54fa3.firebaseio.com',
@@ -9,7 +9,18 @@ const firebaseConfig = {
   measurementId: 'G-4SS7PQK8D5',
 };
 
-export default firebaseConfig;
+const firebaseConfigProd = {
+  apiKey: 'AIzaSyBx8WR38L8eznu6HtwJghjptEN3Ah6HqzE',
+  authDomain: 'akshara-8630e.firebaseapp.com',
+  databaseURL: 'https://akshara-8630e.firebaseio.com',
+  projectId: 'akshara-8630e',
+  storageBucket: 'akshara-8630e.appspot.com',
+  messagingSenderId: '613661168552',
+  appId: '1:613661168552:web:46b655f214b7d0bc18c39d',
+  measurementId: 'G-23MFY9WDH7',
+};
+
+export default firebaseConfigDev;
 
 export const adminEmail = 'rasikadri@gmail.com';
 export const examYears = [
