74ac6fbf1a62195165ac46e90ed72b03f765da2e
Malinda Kumarasinghe <malindakpt@gmail.com>
2020-10-13 00:18:18 +0530

    config changes

--- src/data/Config.ts
+++ src/data/Config.ts
@@ -1,24 +1,24 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyC5pq-lAQWmVEVmrI0iwQ2bUJvSKp4tXaY',
-  authDomain: 'smsm-54fa3.firebaseapp.com',
-  databaseURL: 'https://smsm-54fa3.firebaseio.com',
-  projectId: 'smsm-54fa3',
-  storageBucket: 'smsm-54fa3.appspot.com',
-  messagingSenderId: '794082939900',
-  appId: '1:794082939900:web:9967e6ea91936c2016d879',
-  measurementId: 'G-4SS7PQK8D5',
-};
-
 // const firebaseConfig = {
-//   apiKey: 'AIzaSyBx8WR38L8eznu6HtwJghjptEN3Ah6HqzE',
-//   authDomain: 'akshara-8630e.firebaseapp.com',
-//   databaseURL: 'https://akshara-8630e.firebaseio.com',
-//   projectId: 'akshara-8630e',
-//   storageBucket: 'akshara-8630e.appspot.com',
-//   messagingSenderId: '613661168552',
-//   appId: '1:613661168552:web:46b655f214b7d0bc18c39d',
-//   measurementId: 'G-23MFY9WDH7',
+//   apiKey: 'AIzaSyC5pq-lAQWmVEVmrI0iwQ2bUJvSKp4tXaY',
+//   authDomain: 'smsm-54fa3.firebaseapp.com',
+//   databaseURL: 'https://smsm-54fa3.firebaseio.com',
+//   projectId: 'smsm-54fa3',
+//   storageBucket: 'smsm-54fa3.appspot.com',
+//   messagingSenderId: '794082939900',
+//   appId: '1:794082939900:web:9967e6ea91936c2016d879',
+//   measurementId: 'G-4SS7PQK8D5',
 // };
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBx8WR38L8eznu6HtwJghjptEN3Ah6HqzE',
+  authDomain: 'akshara-8630e.firebaseapp.com',
+  databaseURL: 'https://akshara-8630e.firebaseio.com',
+  projectId: 'akshara-8630e',
+  storageBucket: 'akshara-8630e.appspot.com',
+  messagingSenderId: '613661168552',
+  appId: '1:613661168552:web:46b655f214b7d0bc18c39d',
+  measurementId: 'G-23MFY9WDH7',
+};
 
 export default firebaseConfig;
 
