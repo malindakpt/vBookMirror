ec64ecea4d9ac686ac4c3685dac2db02164255e3
Malinda Kumarasinghe <malindakpt@gmail.com>
2020-10-15 06:27:06 +0530

    Added new build

---
+++ firebase/public/precache-manifest.1396ecb3986411d065ba14877e89583d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fdea21fd95d7cbdba78602edb89f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4a4da5603d75850fa17",
+    "url": "/static/css/main.6d47e2e4.chunk.css"
+  },
+  {
+    "revision": "796400f146a04b522c86",
+    "url": "/static/js/2.f66f2ff0.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.f66f2ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a4da5603d75850fa17",
+    "url": "/static/js/main.db2ac33b.chunk.js"
+  },
+  {
+    "revision": "c5da06a05f547c5738a6",
+    "url": "/static/js/runtime-main.6b9f9885.js"
+  }
+]);
