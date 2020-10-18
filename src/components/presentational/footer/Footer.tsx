837471bb03aebbe880f64f6e11b0fa159d1200fa
Malinda Kumarasinghe <malindakpt@gmail.com>
2020-10-18 12:13:47 +0530

    ADded version to footer

--- src/components/presentational/footer/Footer.tsx
+++ src/components/presentational/footer/Footer.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import classes from './Footer.module.scss';
+import packageJson from '../../../../package.json';
 
-const Footer:React.FC = () => <div className={classes.container}>Powered by AMSI @ 2020</div>;
+const Footer:React.FC = () => (
+  <div className={classes.container}>
+    Powered by AMSI @ 2020
+    -
+    v_
+    {packageJson.version}
+
+  </div>
+);
 
 export default Footer;
