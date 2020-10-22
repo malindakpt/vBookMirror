8cc3e9cff8da3570f46a86843d165b16d53282bb
Malinda Kumarasinghe <malindakpt@gmail.com>
2020-10-22 23:26:53 +0530

    Code refactored

--- src/components/presentational/breadcrumbs/BreadcrumbBar.tsx
+++ src/components/presentational/breadcrumbs/BreadcrumbBar.tsx
@@ -9,7 +9,7 @@ export const BreadcrumbBar = () => {
   return (
     <Breadcrumbs
       aria-label="breadcrumb"
-      style={{ background: '#1d09c8', padding: '10px 20px' }}
+      style={{ background: '#1d09c8', padding: '10px 20px', minHeight: '24px' }}
     >
       {breadcrumbs?.map((bc) => (
         <Link
