f30f1871ae9e7bc47cba905fee26b6dc8e77d5b7
Malinda <malindakpt@gmail.com>
2022-02-24 16:16:12 +0530

    Improvements

--- src/components/presentational/studentPayments/StudentPayments.tsx
+++ src/components/presentational/studentPayments/StudentPayments.tsx
@@ -1,7 +1,7 @@
 import React, {
     useCallback,
     useEffect, useRef, useState,
-  } from 'react';
+} from 'react';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
@@ -9,29 +9,43 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { IPayment } from "../../../interfaces/IPayment";
 import classes from './StudentPayments.module.scss';
+import { displayDate } from '../../../helper/util';
 
 interface Props {
     paymentList: IPayment[];
 }
 
-export const StudentPayments: React.FC<Props> = ({paymentList}) => <Accordion>
-    <AccordionSummary
-      expandIcon={<ExpandMoreIcon />}
-      aria-controls="panel1a-content"
-      id="panel1a-header"
-    >
-      <Typography className={classes.heading}>Attendance Details</Typography>
-    </AccordionSummary>
-    <AccordionDetails>
-      <div>
-        {paymentList.map((payment) => (
-            <div
-              className={classes.container}
-              key={payment.id}
-            >
-              <div>{payment.date}</div>
+export const StudentPayments: React.FC<Props> = ({ paymentList }) => {
+
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        const tot = paymentList.reduce((a, b) => a + b.amount, 0);
+        setTotal(tot);
+    }, [paymentList])
+
+    return <div className={classes.body}><Accordion>
+        <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+        >
+            <Typography className={classes.heading}>Total Payments Value: {total}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <div>
+                {paymentList.map((payment) => (
+                    <div
+                        className={classes.row}
+                        key={payment.id}
+                    >
+                        <div>{displayDate(payment.date)}</div>
+                        <div>{payment.ownerName}</div>
+                        <div>{payment.ownerEmail}</div>
+                        <div>{payment.amount}</div>
+                    </div>
+                ))}
             </div>
-          ))}
-      </div>
-    </AccordionDetails>
-  </Accordion>
+        </AccordionDetails>
+    </Accordion></div>;
+}
